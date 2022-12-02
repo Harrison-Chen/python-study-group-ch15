473b15a779d8e94476e78386be0ea1ed2437489a
Harrison-Chen <41604335+Harrison-Chen@users.noreply.github.com>
2022-12-02 22:55:55 +0800

    Fin my python read group on ch15

---
+++ ch15_multiprocessing.py
@@ -0,0 +1,12 @@
+from multiprocessing import Process
+import os
+
+def whoami(what):
+    print("Process %s says: %s" % (os.getpid(), what))
+
+if __name__ == "__main__": 
+    whoami("I'm the main program") 
+    for n in range(4):
+        p = Process(target=whoami,
+            args=("I'm function %s" % n,))
+        p.start()
